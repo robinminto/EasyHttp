f9e28c4894596c66d9cae6a73d045b7f2bb6cb3b
Robin Minto <robin@minto.co.uk>
2013-02-07 18:10:19 +0000

    Added text/css content type

--- src/EasyHttp/Http/HttpContentTypes.cs
+++ src/EasyHttp/Http/HttpContentTypes.cs
@@ -66,6 +66,7 @@ namespace EasyHttp.Http
         public const string TextPlain = "text/plain";
         public const string TextHtml = "text/html";
         public const string TextCsv = "text/csv";
+        public const string TextCss = "text/css";
         public const string ApplicationJson = "application/json";
         public const string ApplicationXml = "application/xml";
         public const string ApplicationXWwwFormUrlEncoded = "application/x-www-form-urlencoded";
